62f1faad413517b7455790a2c454df262643aecf
khushi-parakh <parakhkhushi45@gmail.com>
2026-07-24 23:33:24 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+list=["Python","Dsa","Pai","Dbms"]
+print("My Subject list=",list)
+list.append("SISM")
+print("After adding new subject",list)
+list.remove("Dsa")
+print("after removing a subject",list)
