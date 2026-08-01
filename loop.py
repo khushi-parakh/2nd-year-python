2afdf1b094ff851eb77044b142857b23f7beb863
khushi-parakh <parakhkhushi45@gmail.com>
2026-08-01 21:19:21 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,20 @@
+
+
+#FOR LOOP PROGRAM
+#the sum program 
+
+sum=0
+k=int(input("Enter the number for the sum is="))
+
+for i in range(1, k+1):
+  sum=sum+i
+
+print("the sum is =", sum)
+
+fact =1
+k=int(input("Enter the number for the factorial  is="))
+
+for i in range(1, k+1):
+  fact=fact*i
+
+print("the factorial is =", fact)
